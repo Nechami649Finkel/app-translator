ff3613a681375046bcd5fb60d905342f2f1a954e
Nechami <39329242515@mby.co.il>
2026-05-12 20:01:04 +0300

    Add state

--- app/terraform/provider.tf
+++ app/terraform/provider.tf
@@ -5,7 +5,20 @@ terraform {
       version = "~> 5.0"
     }
   }
+   # Cerditional state
+  backend "s3" {
+    bucket = "nechami-s3-bucket"
+    key    = "terraform.tfstate"
+    region = "us-east-1" # ודאי שזה זהה לשורה 11
+  }
 }
+
+provider "aws" {
+  region = "us-east-1" # האזור של המשאבים שאת יוצרת
+}
+}
+
+
 
 provider "aws" {
   region = "us-east-1"
