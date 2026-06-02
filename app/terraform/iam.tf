531477fc664fe9ae2944420f13f1e2a9034e8779
Nechami <39329242515@mby.co.il>
2026-06-02 23:14:32 +0300

    Try The CloudFront

--- app/terraform/iam.tf
+++ app/terraform/iam.tf
@@ -1,4 +1,3 @@
-# יצירת הקשר הראשוני בין AWS ל-GitHub
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
@@ -6,7 +5,6 @@ resource "aws_iam_openid_connect_provider" "github" {
   # טביעות האצבע של השרתים של גיטהאב
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
 }
-
 resource "aws_iam_role" "github_actions_role" {
   name = "github-actions-role"
 
@@ -31,23 +29,20 @@ resource "aws_iam_role" "github_actions_role" {
 }
 resource "aws_iam_role_policy" "s3_access_policy" {
   name = "s3-access-policy"
-  role = aws_iam_role.github_actions_role.id # מחבר את ההרשאות לחליפה שיצרנו למעלה
+  role = aws_iam_role.github_actions_role.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
-        Action = [
-          "s3:PutObject",
-          "s3:DeleteObject",
-          "s3:GetObject",
-          "s3:ListBucket"
-        ]
-        Resource = [
-          aws_s3_bucket.frontend.arn,
-          "${aws_s3_bucket.frontend.arn}/*"
-        ]
+        Action   = ["s3:PutObject", "s3:DeleteObject", "s3:GetObject", "s3:ListBucket"]
+        Effect   = "Allow"
+        Resource = [aws_s3_bucket.frontend.arn, "${aws_s3_bucket.frontend.arn}/*"]
+      },
+      {
+        Effect   = "Allow"
+        Action   = "cloudfront:CreateInvalidation"
+        Resource = aws_cloudfront_distribution.s3_distribution.arn
       }
     ]
   })
--- app/terraform/s3.tf
+++ app/terraform/s3.tf
@@ -13,24 +13,27 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
 }
 resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.frontend.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
-resource "aws_s3_bucket_policy" "public_read_access" {
+
+resource "aws_s3_bucket_policy" "allow_cloudfront" {
   bucket = aws_s3_bucket.frontend.id
-  depends_on = [aws_s3_bucket_public_access_block.public_access]
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.frontend.arn}/*"
+    Statement = [{
+      Sid    = "AllowCloudFrontServicePrincipal"
+      Effect = "Allow"
+      Principal = { Service = "cloudfront.amazonaws.com" }
+      Action   = "s3:GetObject"
+      Resource = "${aws_s3_bucket.frontend.arn}/*"
+      Condition = {
+        StringEquals = { 
+          "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn 
+        }
       }
-    ]
+    }]
   })
 }
